e4905552ed29c551404dc6a374b6f8ef9140a20c
WitherTaco <obrushego@gmail.com>
2024-02-28 14:20:39 +0300

    Toothy Bomb (not completed)

---
+++ Content/Items/Weapons/Rogue/FargosCrossover/ToothyBomb.cs
@@ -0,0 +1,41 @@
+﻿using CalamityMod;
+using CalamityMod.Items;
+using CalamityMod.Projectiles.Rogue;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Clamity.Content.Items.Weapons.Rogue.FargosCrossover
+{
+    public class ToothyBomb : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 58;
+            Item.value = CalamityGlobalItem.Rarity4BuyPrice;
+            Item.rare = ItemRarityID.LightRed;
+
+            Item.useTime = Item.useAnimation = 22;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+
+            Item.damage = 35;
+            Item.DamageType = ModContent.GetInstance<RogueDamageClass>();
+            Item.knockBack = 8f;
+
+            Item.shoot = ModContent.ProjectileType<ToothyBombProjectile>();
+            Item.shootSpeed = 12f;
+        }
+    }
+    public class ToothyBombProjectile : BlastBarrelProjectile
+    {
+        public override string Texture => (GetType().Namespace + "." + Name).Replace('.', '/');
+    }
+    public class ToothyBombShrapnel : ModProjectile
+    {
+
+    }
+}
