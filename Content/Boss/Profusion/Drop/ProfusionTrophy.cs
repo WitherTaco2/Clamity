30f92db7da96879c85ccbeff9cc1cfff69471e9d
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2024-03-02 12:47:31 +0300

    Added Profusion Trophy

---
+++ Content/Boss/Profusion/Drop/ProfusionTrophy.cs
@@ -0,0 +1,31 @@
+﻿using CalamityMod.Tiles.Furniture.BossTrophies;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Clamity.Content.Boss.Profusion.Drop
+{
+    public class ProfusionTrophy : ModItem, ILocalizedModType, IModType
+    {
+        public new string LocalizationCategory => "Items.Placeables.Trophies";
+
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 30;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = 50000;
+            Item.rare = ItemRarityID.Blue;
+            Item.createTile = ModContent.TileType<ProfusionTrophyTile>();
+        }
+    }
+    public class ProfusionTrophyTile : CryogenTrophyTile
+    {
+    }
+}
