2acb2f4b7bb7e57813e395a4fb79595cb4159c66
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2024-05-11 17:07:18 +0300

    Squashed commit of the following:
    commit b3854fe931a3ae724ffbd643b3018433bdebea5d
    Author: WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
    Date:   Fri May 3 20:10:05 2024 +0300
    
        Readded Golden Gun from Calamity
    
    commit 24d3592db0702957235df309616f9630ff59b3f7
    Merge: d9350af 1e40d3d
    Author: WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
    Date:   Fri May 3 20:05:10 2024 +0300
    
        Merge branch 'master' into next-calamity-update-adaptation
    
    commit d9350afb9b59eb0b7c730c3fb0b12d3b55a2d67e
    Author: WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
    Date:   Sat Mar 2 10:38:19 2024 +0300
    
        fixes and changes
    
    commit 3c63ad4ab07e7a80801540b8cf3d755245a2e819
    Merge: 02cdcc5 039d05e
    Author: WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
    Date:   Sat Mar 2 08:31:01 2024 +0300
    
        Merge branch 'master' into next-calamity-update-adaptation
    
    commit 02cdcc5d62a8b4d51e8bbb73a0e9dce5c65138fe
    Author: WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
    Date:   Fri Mar 1 20:28:57 2024 +0300
    
        i forgot localization
    
    commit ef4817b4d83584ee86e85f1d356276b4dc686c8a
    Author: WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
    Date:   Fri Mar 1 20:19:45 2024 +0300
    
        I'm ready to update

---
+++ Content/Items/Ammo/AccelerationRound.cs
@@ -0,0 +1,122 @@
+﻿using CalamityMod;
+using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Clamity.Content.Items.Ammo
+{
+    public class AccelerationRound : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Ammo";
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 99;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 10;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 10;
+            Item.height = 16;
+            Item.maxStack = 9999;
+            Item.consumable = true;
+            Item.knockBack = 1.25f;
+            Item.value = Item.sellPrice(copper: 2);
+            Item.rare = ItemRarityID.Green;
+            Item.shoot = ModContent.ProjectileType<AccelerationRoundProj>();
+            Item.shootSpeed = 1f;
+            Item.ammo = AmmoID.Bullet;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(150).
+                AddIngredient(ItemID.MusketBall, 150).
+                AddIngredient<PearlShard>().
+                AddTile(TileID.Anvils).
+                Register();
+        }
+    }
+    public class AccelerationRoundProj : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Ranged";
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 6;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 4;
+            Projectile.height = 4;
+            Projectile.aiStyle = ProjAIStyleID.Arrow;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 600;
+            Projectile.extraUpdates = 3;
+            AIType = ProjectileID.Bullet;
+            Projectile.Calamity().pointBlankShotDuration = CalamityGlobalProjectile.DefaultPointBlankDuration;
+        }
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            Projectile.Clamity().extraAI[0] = Projectile.damage;
+            Projectile.Clamity().extraAI[1] = 1f;
+        }
+
+        public override void AI()
+        {
+            Projectile.velocity *= 1.01f;
+            Projectile.Clamity().extraAI[1] = MathHelper.Clamp(Projectile.Clamity().extraAI[1] * 1.007f, 1, 2);
+            Projectile.damage = (int)(Projectile.Clamity().extraAI[0] * Projectile.Clamity().extraAI[1]);
+            if (Projectile.velocity.Length() >= 12f)
+            {
+                Projectile.velocity *= 0.5f;
+                Projectile.extraUpdates++;
+                Projectile.netUpdate = true;
+            }
+
+            Projectile.localAI[0] += 1f;
+            if (Projectile.localAI[0] > 4f)
+            {
+                if (Main.rand.NextBool(3))
+                {
+                    int num137 = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), 1, 1, 91, 0f, 0f, 0, default, 0.5f);
+                    Main.dust[num137].alpha = Projectile.alpha;
+                    Main.dust[num137].velocity *= 0f;
+                    Main.dust[num137].noGravity = true;
+                }
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
+            return true;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            CalamityUtils.DrawAfterimagesFromEdge(Projectile, 0, lightColor);
+            return false;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+            for (int k = 0; k < 5; k++)
+            {
+                Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, 91, Projectile.oldVelocity.X * 0.5f, Projectile.oldVelocity.Y * 0.5f);
+            }
+        }
+    }
+}
---
+++ Content/Items/Ammo/SuperballBullet.cs
@@ -0,0 +1,106 @@
+﻿using CalamityMod;
+using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Clamity.Content.Items.Ammo
+{
+    public class SuperballBullet : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Ammo";
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 99;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 6;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 8;
+            Item.height = 8;
+            Item.maxStack = 9999;
+            Item.consumable = true;
+            Item.knockBack = 1.5f;
+            Item.value = Item.sellPrice(copper: 2);
+            Item.rare = ItemRarityID.Green;
+            Item.shoot = ModContent.ProjectileType<SuperballBulletProj>();
+            Item.shootSpeed = 1f;
+            Item.ammo = AmmoID.Bullet;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(150).
+                AddIngredient(ItemID.MeteorShot, 150).
+                AddIngredient<SeaRemains>().
+                AddTile(TileID.Anvils).
+                Register();
+        }
+    }
+    public class SuperballBulletProj : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Ranged";
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 4;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 4;
+            Projectile.height = 4;
+            Projectile.aiStyle = ProjAIStyleID.Arrow;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 600;
+            Projectile.extraUpdates = 1;
+            AIType = ProjectileID.Bullet;
+            Projectile.Calamity().pointBlankShotDuration = CalamityGlobalProjectile.DefaultPointBlankDuration;
+        }
+
+        public override void AI()
+        {
+            Projectile.localAI[0] += 1f;
+            if (Projectile.localAI[0] > 4f)
+            {
+                if (Main.rand.NextBool(3))
+                {
+                    int dust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), 1, 1, 87, 0f, 0f, 0, default, 0.5f);
+                    Main.dust[dust].alpha = Projectile.alpha;
+                    Main.dust[dust].velocity *= 0f;
+                    Main.dust[dust].noGravity = true;
+                }
+            }
+
+            if (Projectile.timeLeft == 570 && Projectile.ai[0] == 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center, Projectile.velocity + Main.rand.NextVector2Circular(5, 5), Projectile.type, Projectile.damage / 3, 0.5f, Projectile.owner, 1);
+                }
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            CalamityUtils.DrawAfterimagesFromEdge(Projectile, 0, lightColor);
+            return false;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+            for (int k = 0; k < 5; k++)
+            {
+                Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, 87, Projectile.oldVelocity.X * 0.05f, Projectile.oldVelocity.Y * 0.05f);
+            }
+        }
+    }
+}
