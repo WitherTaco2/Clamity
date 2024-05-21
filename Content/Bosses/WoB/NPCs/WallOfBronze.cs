6f431d6600cacb0d91198ce80f6af6c94ad42370
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2024-05-21 16:24:45 +0300

    a

--- Content/Bosses/WoB/NPCs/WallOfBronze.cs
+++ Content/Bosses/WoB/NPCs/WallOfBronze.cs
@@ -75,8 +75,8 @@ namespace Clamity.Content.Bosses.WoB.NPCs
         });
         public override void ApplyDifficultyAndPlayerScaling(int numPlayers, float balance, float bossAdjustment)
         {
-            NPC.lifeMax = (int)(NPC.lifeMax * 0.5f * balance * bossAdjustment);
-            NPC.damage = (int)(NPC.damage * NPC.GetExpertDamageMultiplier() * bossAdjustment);
+            NPC.lifeMax = (int)(NPC.lifeMax * 0.5f * balance/* * bossAdjustment*/);
+            NPC.damage = (int)(NPC.damage * NPC.GetExpertDamageMultiplier()/* * bossAdjustment*/);
         }
         public override void BossLoot(ref string name, ref int potionType) => potionType = ModContent.ItemType<OmegaHealingPotion>();
         public override void OnSpawn(IEntitySource source)
