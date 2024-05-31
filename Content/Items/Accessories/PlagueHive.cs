986fd1891aac1adb8b0ea6bf76ad770f44f5b2cd
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2024-05-31 21:26:52 +0300

    (Re)added a Plague Hive

---
+++ Content/Items/Accessories/PlagueHive.cs
@@ -0,0 +1,46 @@
+﻿using CalamityMod;
+using CalamityMod.Buffs.DamageOverTime;
+using CalamityMod.CalPlayer;
+using CalamityMod.Items;
+using CalamityMod.Items.Accessories;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Clamity.Content.Items.Accessories
+{
+    public class PlagueHive : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Accessories";
+        public override void SetDefaults()
+        {
+            Item.width = 42;
+            Item.height = 48;
+            Item.value = CalamityGlobalItem.RarityRedBuyPrice;
+            Item.rare = ItemRarityID.Red;
+            Item.accessory = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ItemID.HiveBackpack).
+                AddIngredient<AlchemicalFlask>().
+                AddIngredient(ItemID.HoneyComb).
+                AddIngredient<ToxicHeart>().
+                AddTile(TileID.LunarCraftingStation).
+                Register();
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.strongBees = true;
+            CalamityPlayer modPlayer = player.Calamity();
+            modPlayer.uberBees = true;
+            modPlayer.alchFlask = true;
+            player.buffImmune[ModContent.BuffType<Plague>()] = true;
+
+            ModContent.GetInstance<ToxicHeart>().UpdateAccessory(player, hideVisual);
+        }
+    }
+}
