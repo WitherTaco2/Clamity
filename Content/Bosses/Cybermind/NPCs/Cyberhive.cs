6e12851d5d5b249d309c5a6c727428b37dcae4dd
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2024-11-16 11:28:26 +0300

    Continuing making Cyberhive AI (added enum of attacks and stating AI() ), added Radiactive Blob projectile for future

--- Content/Bosses/Cybermind/NPCs/Cyberhive.cs
+++ Content/Bosses/Cybermind/NPCs/Cyberhive.cs
@@ -1,15 +1,47 @@
 ﻿using CalamityMod;
+using CalamityMod.Events;
+using CalamityMod.NPCs;
+using CalamityMod.World;
+using Microsoft.Xna.Framework;
+using System.IO;
 using Terraria;
+using Terraria.GameContent.Bestiary;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Clamity.Content.Bosses.Cybermind.NPCs
 {
+    public enum CyberhiveAttacks : int
+    {
+        Spawn = 0,
+        //Phase 1
+        Rotating = 1,
+        NormalDash = 2,
+        GasDash = 3,
+
+        //Phase 2
+        HyperVerticalDash = 101,
+        GammaDeathRay = 102,
+        GooDash = 103,
+        ShootVerticalRay = 104,
+    }
     public class Cyberhive : ModNPC
     {
         public static int normalIconIndex;
         public static int phase2IconIndex;
-
+        private int biomeEnrageTimer = CalamityGlobalNPC.biomeEnrageTimerMax;
+        private bool IsPhaseTwo => (NPC.life / (float)NPC.lifeMax) < 0.5f;
+        public int Attacks
+        {
+            get => (int)NPC.ai[0];
+            set => NPC.ai[0] = value;
+        }
+        public CyberhiveAttacks GetAttacks => (CyberhiveAttacks)NPC.ai[0];
+        public int AttacksTimer
+        {
+            get => (int)NPC.ai[1];
+            set => NPC.ai[1] = value;
+        }
         internal static void LoadHeadIcons()
         {
             string normalIconPath = "Clamity/Content/Bosses/Cybermind/NPCs/Cyberhive_Head_Boss";
@@ -45,17 +77,142 @@ namespace Clamity.Content.Bosses.Cybermind.NPCs
             NPC.height = 150;
 
             NPC.defense = 8;
-            NPC.LifeMaxNERB(7700, 9200, 350000);
+            NPC.LifeMaxNERB(100000, 192000, 350000);
             double HPBoost = CalamityConfig.Instance.BossHealthBoost * 0.01;
             NPC.lifeMax += (int)(NPC.lifeMax * HPBoost);
             NPC.aiStyle = -1;
             AIType = -1;
             NPC.knockBackResist = 0f;
-            NPC.value = Item.buyPrice(0, 15, 0, 0);
+            NPC.value = Item.buyPrice(0, 60, 0, 0);
             NPC.boss = true;
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath1;
+
+            NPC.Calamity().VulnerableToElectricity = true;
+            NPC.Calamity().VulnerableToSickness = false;
+            NPC.Calamity().VulnerableToHeat = false;
+            NPC.Calamity().VulnerableToCold = false;
         }
 
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+                BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheCorruption,
+                BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.UndergroundCorruption,
+                BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheCrimson,
+                BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.UndergroundCrimson,
+                new FlavorTextBestiaryInfoElement("Mods.Clamity.NPCs.Cyberhive.Bestiary")
+            });
+        }
+
+        public override void BossHeadSlot(ref int index)
+        {
+            index = IsPhaseTwo ? phase2IconIndex : normalIconIndex;
+        }
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(biomeEnrageTimer);
+        }
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            biomeEnrageTimer = reader.ReadInt32();
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            NPC.frame = new Rectangle(0, IsPhaseTwo ? 136 : 0, 198, 136);
+        }
+
+        public override void AI()
+        {
+            // Get a target
+            if (NPC.target < 0 || NPC.target == Main.maxPlayers || Main.player[NPC.target].dead || !Main.player[NPC.target].active)
+                NPC.TargetClosest();
+
+            Player player = Main.player[NPC.target];
+
+            bool bossRush = BossRushEvent.BossRushActive;
+            bool expertMode = Main.expertMode || bossRush;
+            bool masterMode = Main.masterMode || bossRush;
+            bool revenge = CalamityWorld.revenge || bossRush;
+            bool death = CalamityWorld.death || bossRush;
+
+            // Percent life remaining
+            float lifeRatio = NPC.life / (float)NPC.lifeMax;
+
+            // Enrage
+            /*if ((!player.ZoneCorrupt || !player.ZoneCrimson || (NPC.position.Y / 16f) < Main.worldSurface) && !bossRush)
+            {
+                if (biomeEnrageTimer > 0)
+                    biomeEnrageTimer--;
+            }
+            else
+                biomeEnrageTimer = CalamityGlobalNPC.biomeEnrageTimerMax;
+
+            bool biomeEnraged = biomeEnrageTimer <= 0 || bossRush;
+
+            float enrageScale = bossRush ? 1f : 0f;
+            if (biomeEnraged && (!player.ZoneCorrupt || !player.ZoneCrimson || bossRush))
+            {
+                NPC.Calamity().CurrentlyEnraged = !bossRush;
+                enrageScale += 1f;
+            }
+            if (biomeEnraged && ((NPC.position.Y / 16f) < Main.worldSurface || bossRush))
+            {
+                NPC.Calamity().CurrentlyEnraged = !bossRush;
+                enrageScale += 1f;
+            }*/
+            float enrageScale = bossRush ? 1f : 0f;
+
+            if (IsPhaseTwo)
+            {
+
+            }
+            AttacksTimer++;
+            switch (GetAttacks)
+            {
+                case CyberhiveAttacks.Spawn:
+                    SetNextAttack();
+                    break;
+                case CyberhiveAttacks.Rotating:
+
+                    break;
+                case CyberhiveAttacks.NormalDash:
+
+                    break;
+                case CyberhiveAttacks.GasDash:
+
+                    break;
+
+                case CyberhiveAttacks.HyperVerticalDash:
+
+                    break;
+                case CyberhiveAttacks.GammaDeathRay:
+
+                    break;
+                case CyberhiveAttacks.GooDash:
+
+                    break;
+                case CyberhiveAttacks.ShootVerticalRay:
+
+                    break;
+            }
+        }
+        public void SetNextAttack()
+        {
+            Attacks++;
+            if (IsPhaseTwo)
+            {
+                if (Attacks > 104)
+                    Attacks = 101;
+            }
+            else
+            {
+                if (Attacks > 3)
+                    Attacks = 1;
+
+            }
+            AttacksTimer = 0;
+        }
     }
 }
---
+++ Content/Bosses/Cybermind/Projectiles/RadiactiveBlob.cs
@@ -0,0 +1,137 @@
+﻿using CalamityMod.Projectiles.Boss;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+
+namespace Clamity.Content.Bosses.Cybermind.Projectiles
+{
+    public class RadiactiveBlob : IchorBlob
+    {
+        public override void AI()
+        {
+            if (Projectile.position.Y > Projectile.ai[1] - 48f)
+                Projectile.tileCollide = true;
+
+            // Deal no damage and increment the variable used to kill the projectile after 15 seconds
+            Projectile.localAI[1] += 1f;
+            if (Projectile.localAI[1] > 900f)
+            {
+                Projectile.localAI[0] += 10f;
+                Projectile.damage = 0;
+            }
+
+            // Kill the projectile 26 frames after it stops dealing damage
+            if (Projectile.localAI[0] > 255f)
+            {
+                Projectile.Kill();
+                Projectile.localAI[0] = 255f;
+            }
+
+            // Add yellow light based on alpha
+            Lighting.AddLight(Projectile.Center, (255 - Projectile.alpha) * 0.22f / 255f, (255 - Projectile.alpha) * 0.9f / 255f, (255 - Projectile.alpha) * 0.42f / 255f);
+
+            // Adjust projectile visibility based on the kill timer
+            Projectile.alpha = (int)(100.0 + Projectile.localAI[0] * 0.7);
+
+            if (Projectile.velocity.Y != 0f && Projectile.ai[0] == 0f)
+            {
+                // Rotate based on velocity, only do this here, because it's falling
+                Projectile.rotation = (float)Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X) - MathHelper.PiOver2;
+
+                Projectile.frameCounter++;
+                if (Projectile.frameCounter > 6)
+                {
+                    Projectile.frame++;
+                    Projectile.frameCounter = 0;
+                }
+                if (Projectile.frame > 1)
+                    Projectile.frame = 0;
+            }
+            else
+            {
+                // Prevent sliding
+                Projectile.velocity.X = 0f;
+
+                // Do not animate falling frames
+                Projectile.ai[0] = 1f;
+
+                if (Projectile.frame < 2)
+                {
+                    // Set frame to blob and frame counter to 0
+                    Projectile.frame = 2;
+                    Projectile.frameCounter = 0;
+
+                    // Play squish sound
+                    SoundEngine.PlaySound(SoundID.NPCDeath21, Projectile.Center);
+
+                    // Emit dust
+                    Vector2 dustRotation = (Projectile.rotation - MathHelper.PiOver2).ToRotationVector2();
+                    Vector2 dustVelocity = dustRotation * Projectile.velocity.Length();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int ichorDust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GemEmerald, 0f, 0f, 200, default, 1.6f);
+                        Dust dust = Main.dust[ichorDust];
+                        dust.position = Projectile.Center + Vector2.UnitY.RotatedByRandom(MathHelper.Pi) * (float)Main.rand.NextDouble() * Projectile.width / 2f;
+                        dust.noGravity = true;
+                        dust.velocity.Y -= 2f;
+                        dust.velocity *= 3f;
+                        dust.velocity += dustVelocity * Main.rand.NextFloat();
+                        ichorDust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GemEmerald, 0f, 0f, 100, default, 0.8f);
+                        dust.position = Projectile.Center + Vector2.UnitY.RotatedByRandom(MathHelper.Pi) * (float)Main.rand.NextDouble() * Projectile.width / 2f;
+                        dust.velocity.Y -= 2f;
+                        dust.velocity *= 2f;
+                        dust.noGravity = true;
+                        dust.fadeIn = 1f;
+                        dust.velocity += dustVelocity * Main.rand.NextFloat();
+                    }
+                    for (int j = 0; j < 5; j++)
+                    {
+                        int ichorDust2 = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GemEmerald, 0f, 0f, 0, default, 2f);
+                        Dust dust = Main.dust[ichorDust2];
+                        dust.position = Projectile.Center + Vector2.UnitX.RotatedByRandom(MathHelper.Pi).RotatedBy(Projectile.velocity.ToRotation()) * Projectile.width / 3f;
+                        dust.noGravity = true;
+                        dust.velocity.Y -= 2f;
+                        dust.velocity *= 0.5f;
+                        dust.velocity += dustVelocity * (0.6f + 0.6f * Main.rand.NextFloat());
+                    }
+                }
+
+                Projectile.rotation = 0f;
+                Projectile.gfxOffY = 4f;
+
+                Projectile.frameCounter++;
+                if (Projectile.frameCounter > 6)
+                {
+                    Projectile.frame++;
+                    Projectile.frameCounter = 0;
+                }
+                if (Projectile.frame > 5)
+                    Projectile.frame = 5;
+            }
+
+            // Do velocity code after the frame code, to avoid messing anything up
+            // Reduce x velocity every frame
+            Projectile.velocity.X *= 0.995f;
+
+            // Stop falling if water or lava is hit
+            if (Projectile.wet || Projectile.lavaWet)
+            {
+                Projectile.velocity.Y = 0f;
+            }
+            else
+            {
+                // Fall
+                Projectile.velocity.Y += 0.1f;
+                if (Projectile.velocity.Y > 6f)
+                    Projectile.velocity.Y = 6f;
+            }
+        }
+
+        public override void OnHitPlayer(Player target, Player.HurtInfo info)
+        {
+
+        }
+    }
+}
