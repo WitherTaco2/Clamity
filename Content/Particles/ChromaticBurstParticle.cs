c77b4774c70801f92f817d9410ee22c4f9cbb916
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2025-03-01 19:03:56 +0300

    More particles

--- Content/Particles/ChromaticBurstParticle.cs
+++ Content/Particles/ChromaticBurstParticle.cs
@@ -1,25 +1,19 @@
-﻿using CalamityMod.Particles;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
-using Terraria.ModLoader;
 
-namespace Clamity.Commons.Particles
+namespace Clamity.Content.Particles
 {
-    public class ChromaticBurstParticle : Particle
+    public class ChromaticBurstParticle : BaseClamityParticle
     {
-        public float Opacity = 1f;
-
         public float StartingScale = 1f;
         public float FinalScale = 1f;
 
         public override bool SetLifetime => true;
-
         public override bool UseCustomDraw => true;
-
         public override bool UseAdditiveBlend => true;
 
-        public override string Texture => "CalRemix/Content/Particles/ChromaticBurst";
+        public override string Texture => $"{BaseTexturePath}/ChromaticBurst";
 
         public ChromaticBurstParticle(Vector2 position, Vector2 velocity, Color color, int lifetime, float startingScale, float finalScale)
         {
@@ -41,8 +35,7 @@ namespace Clamity.Commons.Particles
 
         public override void CustomDraw(SpriteBatch spriteBatch)
         {
-            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
-            spriteBatch.Draw(texture, Position - Main.screenPosition, null, Color * Opacity, Rotation, texture.Size() * 0.5f, Scale * 0.3f, 0, 0f);
+            spriteBatch.Draw(TextureFile, Position - Main.screenPosition, null, Color * Opacity, Rotation, TextureFile.Size() * 0.5f, Scale * 0.3f, 0, 0f);
         }
     }
 }
